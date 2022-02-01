c1d8ae3d0b01c7bd15ccbe99693aa061139673e0
hsnbsry <basriakbas@hotmail.com>
2022-02-01 11:16:43 +0100

    1Feb

--- src/test/java/stepdefinitions/Twitter.java
+++ src/test/java/stepdefinitions/Twitter.java
@@ -2,9 +2,9 @@ package stepdefinitions;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+
 import pages.Twitterpage;
 import utilities.Driver;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -13,6 +13,7 @@ public class Twitter {
     Twitterpage page=new Twitterpage();
    static String dlkur="";
     static String eurokur="";
+
 
     @Given("benutzer geht {string} internet seite")
     public void benutzer_geht_internet_seite(String string) {
@@ -23,7 +24,7 @@ public class Twitter {
     }
 
     @Given("benutzer bekommt dollar preis")
-    public void benutzer_bekommt_dollar_preis() {
+    public void benutzer_bekommt_dollar_preis()  {
        dlkur=page.dolarKur.getText();
         System.out.println(dlkur);
     }
@@ -37,18 +38,20 @@ public class Twitter {
     @Given("benutzer geht {string} twitter internet seite")
     public void benutzer_geht_twitter_internet_seite(String string) {
         Driver.getDriver().get(string);
-        Driver.wait(2);
+        Driver.wait(3);
     }
 
     @Given("benutzer sendet benutzername {string}")
     public void benutzer_sendet_benutzername(String string) {
-        page.username.sendKeys(string);
         Driver.wait(2);
+        page.username.sendKeys(string);
+
     }
 
     @Given("benutzer drückt weiterButton")
     public void benutzer_drückt_weiterButton() {
         page.weiterButton.click();
+        Driver.wait(3);
     }
 
     @Given("benutzer sendet passwort {string}")
